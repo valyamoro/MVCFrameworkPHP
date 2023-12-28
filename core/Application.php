a7a94c31ee21eeb3e24e265fb6e5ce1bd126ce00
kutlumbek <Moralek228@gmail.com>
2023-12-28 09:34:21 +0300

    Добавил обработку пост запросов и html страниц

--- core/Application.php
+++ core/Application.php
@@ -4,16 +4,23 @@ namespace app\core;
 
 class Application
 {
+    public static string $rootDir;
     public Router $router;
     public Request $request;
-    public function __construct()
+    public Response $response;
+    public static Application $app;
+
+    public function __construct(string $rootPath)
     {
+        self::$rootDir = $rootPath;
+        self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
     }
 
     public function run(): void
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
